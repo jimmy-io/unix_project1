4e58850569bd47bb045f03c9fca21ace5afc70be
jimmy-io <jimmyjoy@usc.edu>
2019-06-21 13:44:10 -0700

    modified the game  code

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,29 +7,14 @@ echo "You guessed $var_num"
 
 num_files=$(ls -1 | wc -l)
 
-#echo "number of files in the current director is $num_files"
-
-#if [[ $var_num -eq $num_files ]]
-#then
-#	echo "Congratulations! You guessed right! the number of files is $num_files"
-#else
-#	echo "You guessed incorrectly "
-#	if  [[ $var_num -gt $num_files ]]
-#	then
-#		echo "Your guess was too high. Guess again?"
-#	else 
-#		echo "Your guess was too low. Guess again?"
-#	fi
-#fi
-
 until [[ $var_num -eq $num_files ]]
 do
 	echo "You guessed wrong!"
 		if  [[ $var_num -gt $num_files ]]
         	then
-                	echo "Your guess was too high. Guess again?"
+                	echo "Your guess was too high"
         	else
-                	echo "Your guess was too low. Guess again?"
+                	echo "Your guess was too low"
         	fi
 	echo "Guess again?"
 	read var_num
